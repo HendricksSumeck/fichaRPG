b46c5eed1c884ae63fac07f090d30ed734d979f9
gabrielimachado <gabrielipereiramachado2009@hotmail.com>
2019-05-24 19:29:29 -0300

    começo

---
+++ dev/inicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>dragon5e</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+
+
+</head>
+<body id="inicio">
+<div id="entrar" class="">
+  <a href= "/web/Novapasta/fichaRPG/dev/login/"><button type="button" class="btn btn-danger">Entrar</button></a>;
+  <a href= "/web/Novapasta/fichaRPG/dev/registrar/"><button type="button" class="btn btn-danger">Cadastrar</button></a>;
+
+</div>
+
+<form>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+
+</body>
+
+
+</html>
