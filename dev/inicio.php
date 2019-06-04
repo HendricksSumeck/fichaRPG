dfbfdf4b6eb1f1bb3d9df29b6d652315bcc07759
J.Otávio <bremm@aluno.utfpr.com.br>
2019-06-04 09:44:08 -0300

    att no front

--- dev/inicio.php
+++ dev/inicio.php
@@ -7,26 +7,35 @@
 
 </head>
 <body id="inicio">
-<div id="entrar" class="">
-  <a href= "/web/Novapasta/fichaRPG/dev/login/"><button type="button" class="btn btn-danger">Entrar</button></a>;
-  <a href= "/web/Novapasta/fichaRPG/dev/registrar/"><button type="button" class="btn btn-danger">Cadastrar</button></a>;
+    <div id="entrar" class="">
+        <a href= "/web/Novapasta/fichaRPG/dev/login/"></a>
+        <a href= "/web/Novapasta/fichaRPG/dev/registrar/"></a>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-</div>
+        <!--SCRIPTS JS DO BOOTSTRAP -->
 
-<form>
-  <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
-  </div>
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
+
+    </div>
+
+    <form id="login">
+      <div class="form-group">
+        <label for="email">Email address:</label>
+        <input type="email" class="form-control" id="email">
+      </div>
+
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd">
+      </div>
+
+        <button type="button" class="btn btn-danger">Entrar</button>
+        <button id="bCadastrar"type="button" class="btn btn-danger">Cadastrar</button>
+    </form>
+
+
 
 
 </body>
