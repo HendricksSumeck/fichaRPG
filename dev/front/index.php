84616457661eb2b2fa5b695fd1cd764030ba7ed9
gabrielimachado <gabrielipereiramachado2009@hotmail.com>
2019-06-05 19:37:43 -0300

    att

--- dev/front/index.php
+++ dev/front/index.php
@@ -13,8 +13,42 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
 </head>
 
-<body>
+<body id="inicio">
+    <div id="entrar" class="">
+        <a href= "/web/Novapasta/fichaRPG/dev/login/"></a>
+        <a href= "/web/Novapasta/fichaRPG/dev/registrar/"></a>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
+        <!--SCRIPTS JS DO BOOTSTRAP -->
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
+
+    </div>
+    
+
+    
+<div class="container" id="containerLogin">
+    <div class="row">
+        <div class="col-md-3">
+            <form id="login">
+              <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email">
+              </div>
+
+              <div class="form-group">
+                <label for="pwd">Senha:</label>
+                <input type="password" class="form-control" id="pwd">
+              </div>
+
+                <a href="#" type="submit" class="btn btn-danger">Entrar</a>
+                <a href="registrar.php" id="bCadastrar" type="button" class="btn btn-danger">Cadastrar</a>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 
