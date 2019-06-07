013b93dba0d3ae6009359e3cbfbd6945da8e5b4d
gabrielimachado <gabrielipereiramachado2009@hotmail.com>
2019-06-06 22:36:48 -0300

    novo inicio

--- dev/front/inicio.php
+++ dev/front/inicio.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>dragon5e</title>
-    <link rel="stylesheet" type="text/css" href="estilo.css">
+  <head>
+   	<title>dragon5e</title>
+	<!--LINK CSS-->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/estilo.css"/>
+
+	<!--LINK DO BOOTSTRAP CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!--SCRIPTS JS DO BOOTSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
 
 </head>
 <body id="bodyini">
@@ -13,11 +21,15 @@
       <h5 class="text-white h4">Collapsed content</h5>
       <span class="text-muted">Toggleable via the navbar brand.</span>
     </div>
-  </div>-->
+  </div>
   <nav class="navbar navbar-dark bg-dark" >
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
+    </button>-->
+
+    <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+</nav>
 </div>
 </body>
 
