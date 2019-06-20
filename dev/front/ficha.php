d91a43873c550694e17cac5d61a3d1201be78d0a
gabrielimachado <gabrielipereiramachado2009@hotmail.com>
2019-06-19 23:38:49 -0300

    ficha em andamento

--- dev/front/ficha.php
+++ dev/front/ficha.php
@@ -3,7 +3,7 @@
 <head>
 	<title></title>
 	<!--LINK CSS-->
-	<link rel="stylesheet" type="text/css" media="screen" href=""/>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/estilo.css"/>
 
 	<!--LINK DO BOOTSTRAP CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
@@ -12,7 +12,137 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
 </head>
-<body>
+<body id="bodyficha">
+
+<div class="row">
+ <div class="form-group col-md-6 px-5 pt-5">
+   <label for="campo1">Nome do personagem:</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+
+ <div class="form-group col-md-2 px-5 pt-3">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5 pt-3">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ <div class="form-group col-md-2 px-5 pt-3">
+   <label for="campo1">Campo Um</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+
+</div>
+
+<input type="text" class="form-control" id="campo3">
+
+ 
+<div class="row pt-3">
+ <div class="form-group col-md-2 px-5">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ <div class="form-group col-md-2 px-5">
+   <label for="campo1">Campo Um</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+ 
+
+
+</div>
+
+
+<div class="row pl-5">
+ <div class="form-group col-md-2 px-5">
+   <label for="campo1">Campo Um</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+</div>
+
+
+
+
+<div class="row">
+<div class="form-group col-md-2 px-5">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+
+ <div class="form-group col-md-2 px-5">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+
+ <div class="form-group col-md-2 px-5">
+   <label for="campo1">Campo Um</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+ 
+
+
+</div>
+
+
+<div class="row">
+ <div class="form-group col-md-2 px-5">
+   <label for="campo1">Campo Um</label>
+   <input type="text" class="form-control" id="campo1">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo2">Campo Dois</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+ 
+ <div class="form-group col-md-2 px-5">
+   <label for="campo3">Campo Três</label>
+   <input type="text" class="form-control" id="campo3">
+ </div>
+</div>
+
+
+<form action="index.html0" class="col-md-12 px-5">
+  <!-- area de campos do form -->
+  <hr />
+  <div id="actions" class="row">
+    <div class="col-md-8">
+      <button type="submit" class="btn btn-primary">Salvar</button>
+      <a href="index.html" class="btn btn-default">Cancelar</a>
+    </div>
+  </div>
+</form>
+
+
+
+
+
 
 </body>
 </html>
