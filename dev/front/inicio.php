27a041c65412ad78c2e0154a95141e8f24e5e764
gabrielimachado <gabrielipereiramachado2009@hotmail.com>
2019-06-22 01:53:34 -0300

    inicio

--- dev/front/inicio.php
+++ dev/front/inicio.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
-  <head>
-   	<title>dragon5e</title>
-	<!--LINK CSS-->
-    <link rel="stylesheet" type="text/css" media="screen" href="css/estilo.css"/>
 
-	<!--LINK DO BOOTSTRAP CSS-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <!--SCRIPTS JS DO BOOTSTRAP -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
+<head>
+  <title>dragon5e</title>
+  <!--LINK CSS-->
+  <link rel="stylesheet" type="text/css" media="screen" href="css/estilo.css" />
+
+  <!--LINK DO BOOTSTRAP CSS-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <!--SCRIPTS JS DO BOOTSTRAP -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
 </head>
+
 <body id="bodyini">
 
-<div class="pos-f-t">
- <!-- <div class="collapse" id="navbarToggleExternalContent">
+  <!-- <div class="pos-f-t">
+     <div class="collapse" id="navbarToggleExternalContent">
     <div class="bg-dark p-4">
       <h5 class="text-white h4">Collapsed content</h5>
       <span class="text-muted">Toggleable via the navbar brand.</span>
@@ -25,13 +27,72 @@
   <nav class="navbar navbar-dark bg-dark" >
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>-->
+    </button>
 
     <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-</nav>
-</div>
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+
+  </div>
+
+  <nav class="nav navbar-expand-lg justify-content-between d-flex" id="nav">
+
+    <a class="navbar-brand" href="bodyini"></a>
+  </nav>-->
+
+  <nav class="nav navbar-expand-lg justify-content-between d-flex" id="">
+        <div class="logo">
+            <a href="inicio"><img id="logo" src="front/img/logo.png"></a>
+        </div>
+        <div id="entrar" class=" pt-2">
+            <a href="/web/Novapasta/fichaRPG/dev/front/index/"><button class="btn btn-outline-dark ">Sair</button></a>
+        </div>
+    </nav>
+
+  <div class="col-sm-12 row py-4 ">
+
+    <div class="col-lg-8 py-5 offset-2 row">
+      <div class="pt-5">
+        <button type="button" class="btn btn-secondary btn-lg text-center ">Adicionar ficha</button>
+      </div>
+
+      <div class="pl-4">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="../front/img/lucasbolas.png " alt="Card image cap"width="200px" height="200px">
+          <div class=" card-body">
+            <h5 class="card-title text-center">Lucas3bolas</h5>
+            <a href="#" class="btn btn-primary col-sm-12">Visualizar ficha</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="pl-4" >
+        <div class="card" style="width: 18rem;">
+           <img class="card-img-top" src="../front/img/paladino.png" alt="Card image cap" width="200px" height="200px">
+            <div class=" card-body">
+              <h5 class="card-title text-center">Rubinho</h5>
+              <a href="#" class="btn btn-primary col-sm-12">Visualizar ficha</a>
+            </div>
+          </div>
+        </div>
+    </div>
+  
 </body>
 
 
 </html>
+
+<!--<div class="row">              <p class="card-text">Paladino/descrição</p>
+
+        <img class="offset-10 pt-3" src="../front/img/lucasbolas.png" alt="" width="250px" height="250px">
+        <div class="pt-2 offset-11 pr-3">
+          <span class='badge badge-dark offset-9 pr-2 pl-2'>Lucas3bolas</span>
+        </div>
+      </div>
+      <div class="row">
+      <img class="offset-10 pt-3" src="../front/img/paladino.png" alt="" width="250px" height="250px">
+      <div class="pt-2 offset-11 pr-2">
+        <span class='badge badge-dark offset-11 pr-2 pl-2'>Rubinho</span>
+      </div>
+    </div>
+    </div>-->
